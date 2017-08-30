c3efdd49f07a4500b6f38d578e516dca16490eff
ChuckBTaylor <WLScripps@gmail.com>
2017-08-30 17:27:28 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+class School
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, num)
+    if !(self.roster.has_key?(num))
+      self.roster[num] = []
+    end
+    self.roster[num] << name
+  end
+
+  def grade(num)
+    self.roster[num]
+  end
+
+  def sort
+    self.roster.sort.to_h.map do |grade, students|
+      students.sort!
+    end
+    self.roster
+  end
+
+end
